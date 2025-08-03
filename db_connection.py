6b237ab79a16b7b387fcf5bc446339a2def86979
zia <chinese.zhaoziye@gmail.com>
2025-08-03 23:44:10 +0930

    add reserved_words checking

--- db_connection.py
+++ db_connection.py
@@ -3,6 +3,46 @@ import sqlite3
 
 import pandas as pd
 
+def check_reserved_word(word):
+
+    words = [reserved_words_sqlite]
+
+    for w in words:
+        if word.lower() in w:
+            return f'"{words}"'
+    return None
+
+
+# SQLite
+
+reserved_words_sqlite = [
+    "ABORT", "ACTION", "ADD", "AFTER", "ALL", "ALTER", "ANALYZE",
+    "AND", "AS", "ASC", "ATTACH", "AUTOINCREMENT",
+    "BEFORE", "BEGIN", "BETWEEN", "BY", "CASCADE",
+    "CASE", "CAST", "CHECK", "COLLATE", "COLUMN",
+    "COMMIT", "CONFLICT", "CONSTRAINT", "CREATE", "CROSS",
+    "CURRENT_DATE", "CURRENT_TIME", "CURRENT_TIMESTAMP",
+    "DATABASE", "DEFAULT", "DEFERRABLE", "DEFERRED", "DELETE",
+    "DESC", "DETACH", "DISTINCT", "DO", "DROP",
+    "EACH", "ELSE", "END", "ESCAPE", "EXCEPT",
+    "EXCLUSIVE", "EXISTS", "EXPLAIN", "FAIL", "FOR",
+    "FOREIGN", "FROM", "FULL", "GLOB", "GROUP",
+    "HAVING", "IF", "IGNORE", "IMMEDIATE", "IN",
+    "INDEX", "INDEXED", "INITIALLY", "INNER", "INSERT",
+    "INSTEAD", "INTERSECT", "INTO", "IS", "ISNULL",
+    "JOIN", "KEY", "LEFT", "LIKE", "LIMIT",
+    "MATCH", "NATURAL", "NO", "NOT", "NOTHING",
+    "NULL", "OF", "OFFSET", "ON", "OR",
+    "ORDER", "OUTER", "PLAN", "PRAGMA", "PRIMARY",
+    "QUERY", "RAISE", "RECURSIVE", "REFERENCES", "REGEXP",
+    "REINDEX", "RELEASE", "RENAME", "REPLACE",
+    "RESTRICT", "RIGHT", "ROLLBACK", "ROW", "SAVEPOINT",
+    "SELECT", "SET", "TABLE", "TEMP", "TEMPORARY",
+    "THEN", "TIES", "TO", "TRANSACTION", "TRIGGER",
+    "UNION", "UNIQUE", "UPDATE", "USING", "VACUUM",
+    "VALUES", "VIEW", "VIRTUAL", "WHEN", "WHERE",
+    "WITH", "WITHOUT"
+]
 
 def infer_sql_type(value):
     if pd.isnull(value):

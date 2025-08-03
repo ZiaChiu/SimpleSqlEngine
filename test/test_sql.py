513d58a5e38d8ad57cb6bcb0d93936da1912bfaf
zia <chinese.zhaoziye@gmail.com>
2025-08-04 04:51:45 +0930

    updated test

--- test/test_sql.py
+++ test/test_sql.py
@@ -58,73 +58,57 @@ class TestSQLite:
             .select("index")
         assert query.build() == 'SELECT "index" FROM NetflixTVShowsAndMovies;'
 
-# query = (
-#     SQLQueryBuilder("NetflixTVShowsAndMovies")
-#     .select("type", ('AVG("imdb_score")', "avg_score"))
-#     .where("type", False)  # Genre IS NOT NULL
-#     .and_("imdb_score", (">=", 7))
-#     .group_by("type")
-#     .order_by("avg_score", desc=True)
-#     .limit(5)
-#     .build()
-# )
-# d = DataOutput("my_database.db", query,"results")
-# c = d.get_csv()
-# print(c)
-#
-# d.line_plot(kind="scatter")
-#
-# def test_demo():
-#     assert 100 == 100
+    def test_query_select_with_reserved_word_in_function(self):
+        query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
+            .select(("AVG(index)", "avg_index"))
+        assert query.build() == 'SELECT AVG("index") AS avg_index FROM NetflixTVShowsAndMovies;'
+    def test_query_select_with_reserved_word_in_function_and_alias(self):
+        query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
+            .select(("AVG(index)", "avg_index"), ("MIN(index)", "min_index"))
+        assert query.build() == 'SELECT AVG("index") AS avg_index, MIN("index") AS min_index FROM NetflixTVShowsAndMovies;'
+    def test_query_select_with_reserved_word_in_function_and_alias_and_where(self):
+        query = (SQLQueryBuilder("NetflixTVShowsAndMovies")
+                 .select(("AVG(index)", "avg_index"))
+                 .where("type", False)
+                 .and_("index", (">=", 7))
+                 .group_by("type")
+                 .order_by("avg_index", desc=True)
+                 .limit(5))
 
-# class TestDome:
-#
-#     def test_demo1(self):
-#         assert 11 == 11
-#
-#     def test_demo(self):
-#         assert 22 == 21
-# class TestDome:
-#
-#     def test_demo1(self):
-#         print('\n----测试用例执行-----------')
-#         assert 11 == 11
+        assert query.build() == 'SELECT AVG("index") AS avg_index FROM NetflixTVShowsAndMovies WHERE type IS NOT NULL AND "index" >= 7 GROUP BY type ORDER BY avg_index DESC LIMIT 5;'
+    def test_query_exists(self):
+        query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
+            .select("index") \
+            .exists_(SQLQueryBuilder("NetflixTVShowsAndMovies").select("index").build())
 
-# query = (
-# SQLQueryBuilder("NetflixTVShowsAndMovies")
-# .select("release_year","imdb_score")
-# .where("age_certification", "R")
-# .build()
-#
-# )
-#
-# query1 = (
-# SQLQueryBuilder("NetflixTVShowsAndMovies")
-# .update(release_year=2026, imdb_score=8.5)
-# .where("index", 3)
-# .build()
-# )
+        assert query.build() == 'SELECT "index" FROM NetflixTVShowsAndMovies WHERE EXISTS (SELECT "index" FROM NetflixTVShowsAndMovies);'
+    def test_query_exists_with_reserved_word(self):
+        query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
+            .select("index") \
+            .exists_(SQLQueryBuilder("NetflixTVShowsAndMovies").select("index").build())
 
+        assert query.build() == 'SELECT "index" FROM NetflixTVShowsAndMovies WHERE EXISTS (SELECT "index" FROM NetflixTVShowsAndMovies);'
+    def test_query_exists_with_reserved_word_in_function(self):
+        query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
+            .select(("AVG(index)", "avg_index")) \
+            .exists_(SQLQueryBuilder("NetflixTVShowsAndMovies").select("index").build())
 
-# UPDATE NetflixTVShowsAndMovies SET "release_year" = 2026,
-# "imdb_score" = 8.5 WHERE "index" IS NULL;
-# print(query1)
+        assert query.build() == 'SELECT AVG("index") AS avg_index FROM NetflixTVShowsAndMovies WHERE EXISTS (SELECT "index" FROM NetflixTVShowsAndMovies);'
+    def test_query_exists_with_reserved_word_in_function_and_alias(self):
+        query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
+            .select(("AVG(index)", "avg_index"), ("MIN(index)", "min_index")) \
+            .exists_(SQLQueryBuilder("NetflixTVShowsAndMovies").select("index").build())
 
+        assert query.build() == 'SELECT AVG("index") AS avg_index, MIN("index") AS min_index FROM NetflixTVShowsAndMovies WHERE EXISTS (SELECT "index" FROM NetflixTVShowsAndMovies);'
+    def test_query_exists_with_reserved_word_in_function_and_alias_and_where(self):
+        query = (SQLQueryBuilder("NetflixTVShowsAndMovies")
+                 .select(("AVG(index)", "avg_index"))
+                 .where("type", False)
+                 .and_("index", (">=", 7))
+                 .group_by("type")
+                 .order_by("avg_index", desc=True)
+                 .limit(5)
+                 .exists_(SQLQueryBuilder("NetflixTVShowsAndMovies").select("index").build()))
 
-# d = DataOutput("./my_database.db", query1,"results4")
-# c = d.get_csv()
-# print(c)
+        assert query.build() == 'SELECT AVG("index") AS avg_index FROM NetflixTVShowsAndMovies WHERE type IS NOT NULL AND "index" >= 7 GROUP BY type ORDER BY avg_index DESC LIMIT 5 AND EXISTS (SELECT "index" FROM NetflixTVShowsAndMovies);'
 
-
-# def test_plt():
-#     p = plt.figure(figsize=(10, 6))
-#     return p
-
-
-# test_plt()
-# p = test_plt()
-# p.line_plot(kind="line", title="IMDB Score by Release Year for R Rated Movies")
-# plt.xlabel("Release Year")
-# plt.ylabel("IMDB Score")
-# plt.title("IMDB Score by Release Year for R Rated Movies")
-# plt.show()

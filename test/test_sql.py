11cf963009e4af0bb259c46c77258b7081b27ea6
zia <chinese.zhaoziye@gmail.com>
2025-08-04 04:02:42 +0930

    update test

--- test/test_sql.py
+++ test/test_sql.py
@@ -23,12 +23,40 @@ class TestSQLite:
     def test_query_select(self):
         query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
             .select("index")
-        assert query.build() == 'SELECT "index" FROM "NetflixTVShowsAndMovies"'
+        print("Q:",query)
+        assert query.build() == 'SELECT "index" FROM NetflixTVShowsAndMovies;'
     def test_query_select_with_alias(self):
         query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
             .select("index", ("imdb_score", "score"))
-        assert query.build() == 'SELECT "index", "imdb_score" AS "score" FROM "NetflixTVShowsAndMovies"'
+        assert query.build() == 'SELECT "index", imdb_score AS score FROM NetflixTVShowsAndMovies;'
+    def test_query_select_with_function(self):
+        query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
+            .select(("AVG(imdb_score)", "avg_score"))
+        assert query.build() == 'SELECT AVG(imdb_score) AS avg_score FROM NetflixTVShowsAndMovies;'
+    def test_query_select_with_function_and_alias(self):
+        query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
+            .select(("AVG(imdb_score)", "avg_score"), ("MIN(imdb_score)", "min_score"))
+        assert query.build() == 'SELECT AVG(imdb_score) AS avg_score, MIN(imdb_score) AS min_score FROM NetflixTVShowsAndMovies;'
+    def test_query_select_with_function_and_alias_and_where(self):
+        query = (SQLQueryBuilder("NetflixTVShowsAndMovies")
+                 .select(("AVG(imdb_score)", "avg_score"))
+                 .where("type", False)
+                 .and_("imdb_score", (">=", 7))
+                 .group_by("type")
+                 .order_by("avg_score", desc=True)
+                 .limit(5))
 
+        assert query.build() == 'SELECT AVG(imdb_score) AS avg_score FROM NetflixTVShowsAndMovies WHERE type IS NOT NULL AND imdb_score >= 7 GROUP BY type ORDER BY avg_score DESC LIMIT 5;'
+    def test_query_update(self):
+        query = (SQLQueryBuilder("NetflixTVShowsAndMovies")
+                 .update(release_year=2026, imdb_score=8.5)
+                 .where("index", 3))
+
+        assert query.build() == 'UPDATE NetflixTVShowsAndMovies SET "release_year" = 2026, "imdb_score" = 8.5 WHERE "index" = 3;'
+    def test_query_select_with_reserved_word(self):
+        query = SQLQueryBuilder("NetflixTVShowsAndMovies") \
+            .select("index")
+        assert query.build() == 'SELECT "index" FROM NetflixTVShowsAndMovies;'
 
 # query = (
 #     SQLQueryBuilder("NetflixTVShowsAndMovies")
